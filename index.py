cade423ed21800f13b87964d86f583a45cbf478a
Ali Haider <45093565+alihaider589@users.noreply.github.com>
2021-03-07 00:22:10 +0500

    Create index.py
    adding py file

---
+++ index.py
@@ -0,0 +1,5 @@
+print('Hello and welcome to the band name generator\n')
+city=input('Kindly tell which city you grew up in\n')
+pet=input('Tell us which was the name of your first pet\n')
+
+print('The name for your band could be' +pet+city)
